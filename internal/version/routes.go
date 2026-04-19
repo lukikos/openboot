cac10c4e891acdd6aec9a463ae6ba9a836b17c9f
Luki Kos <lukikos@users.noreply.github.com>
2026-04-20 00:18:27 +0800

    feat: add /v as a short alias for the /version endpoint

--- internal/version/routes.go
+++ internal/version/routes.go
@@ -3,6 +3,8 @@ package version
 import "net/http"
 
 // RegisterRoutes registers version endpoints on the given mux.
+// Also registers /v on a shorter alias for convenience.
 func RegisterRoutes(mux *http.ServeMux) {
 	mux.HandleFunc("/version", Handler)
+	mux.HandleFunc("/v", Handler)
 }

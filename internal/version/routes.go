6454d8b63e88e86b03f170e5978ff71b1186e820
Luki Kos <lukikos@users.noreply.github.com>
2026-04-23 23:12:43 +0800

    feat: add /info as additional version endpoint alias

--- internal/version/routes.go
+++ internal/version/routes.go
@@ -5,8 +5,10 @@ import "net/http"
 // RegisterRoutes registers version endpoints on the given mux.
 // Also registers /v on a shorter alias for convenience.
 // Personal note: also adding /ver as an additional alias.
+// Personal note: added /info as a more descriptive alias.
 func RegisterRoutes(mux *http.ServeMux) {
 	mux.HandleFunc("/version", Handler)
 	mux.HandleFunc("/v", Handler)
 	mux.HandleFunc("/ver", Handler)
+	mux.HandleFunc("/info", Handler)
 }
